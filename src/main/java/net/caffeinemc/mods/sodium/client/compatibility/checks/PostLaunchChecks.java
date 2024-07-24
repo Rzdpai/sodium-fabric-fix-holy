2e22b1dbafa65728ec4d319d2c24346bc2669486
认真的π <79180302+Rzdpai@users.noreply.github.com>
2024-07-24 11:49:27 +0800

    Add files via upload
    回退

--- src/main/java/net/caffeinemc/mods/sodium/client/compatibility/checks/PostLaunchChecks.java
+++ src/main/java/net/caffeinemc/mods/sodium/client/compatibility/checks/PostLaunchChecks.java
@@ -1,57 +1,47 @@
-package net.caffeinemc.mods.sodium.client.compatibility.checks;
+//
+// Decompiled by Jadx - 4453ms
+//
+package me.jellysquid.mods.sodium.client.compatibility.checks;
 
-import net.caffeinemc.mods.sodium.client.gui.console.Console;
-import net.caffeinemc.mods.sodium.client.gui.console.message.MessageLevel;
-import net.minecraft.network.chat.Component;
+import me.jellysquid.mods.sodium.client.gui.console.Console;
+import me.jellysquid.mods.sodium.client.gui.console.message.MessageLevel;
+import net.minecraft.class_2561;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-/**
- * Performs OpenGL driver validation after the game creates an OpenGL context. This runs immediately after OpenGL
- * context creation, and uses the implementation details of the OpenGL context to perform validation.
- */
 public class PostLaunchChecks {
     private static final Logger LOGGER = LoggerFactory.getLogger("Sodium-PostlaunchChecks");
 
     public static void onContextInitialized() {
-        // FIXME: This can be determined earlier, but we can't access the GUI classes in pre-launch
         if (isUsingPojavLauncher()) {
-        return flase;
+            Console.instance().logMessage(MessageLevel.SEVERE, class_2561.method_43471("sodium.console.pojav_launcher"), 30.0d);
+            LOGGER.error("It appears that PojavLauncher is being used with an OpenGL compatibility layer. This will likely cause severe performance issues, graphical issues, and crashes when used with Sodium. This configuration is not supported -- you are on your own!");
         }
     }
 
-    // https://github.com/CaffeineMC/sodium-fabric/issues/1916
     private static boolean isUsingPojavLauncher() {
         if (System.getenv("POJAV_RENDERER") != null) {
             LOGGER.warn("Detected presence of environment variable POJAV_LAUNCHER, which seems to indicate we are running on Android");
-
             return true;
         }
-
-        var librarySearchPaths = System.getProperty("java.library.path", null);
-
-        if (librarySearchPaths != null) {
-            for (var path : librarySearchPaths.split(":")) {
-                if (isKnownAndroidPathFragment(path)) {
-                    LOGGER.warn("Found a library search path which seems to be hosted in an Android filesystem: {}", path);
-
+        String property = System.getProperty("java.library.path", null);
+        if (property != null) {
+            String[] split = property.split(":");
+            for (String str : split) {
+                if (isKnownAndroidPathFragment(str)) {
+                    LOGGER.warn("Found a library search path which seems to be hosted in an Android filesystem: {}", str);
                     return true;
                 }
             }
         }
-
-        var workingDirectory = System.getProperty("user.home", null);
-
-        if (workingDirectory != null) {
-            if (isKnownAndroidPathFragment(workingDirectory)) {
-                LOGGER.warn("Working directory seems to be hosted in an Android filesystem: {}", workingDirectory);
-            }
+        String property2 = System.getProperty("user.home", null);
+        if (property2 != null && isKnownAndroidPathFragment(property2)) {
+            LOGGER.warn("Working directory seems to be hosted in an Android filesystem: {}", property2);
         }
-
         return false;
     }
 
-    private static boolean isKnownAndroidPathFragment(String path) {
-        return path.matches("/data/user/[0-9]+/net\\.kdt\\.pojavlaunch");
+    private static boolean isKnownAndroidPathFragment(String str) {
+        return str.matches("/data/user/[0-9]+/net\\.kdt\\.pojavlaunch");
     }
 }
